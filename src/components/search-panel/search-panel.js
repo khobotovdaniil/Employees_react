4cbce61cc81cf1a938e3f712b22c069c464743ae
Daniil_Khobotov <dinich44@gmail.com>
2022-11-05 12:49:13 +0400

    add carcass

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input 
+            type="text"
+            className="form-control search-input"
+            placeholder="Haйти сотрудника" />
+    )
+}
+
+export default SearchPanel;
